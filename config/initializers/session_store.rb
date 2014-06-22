11354a68e95025c5c8ca6cb1d640f0377ed1d574
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-06-22 20:19:35 +0100

    Finished basic template setup.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Releasr::Application.config.session_store :cookie_store, key: '_releasr_session'
---
+++ spec/support/web_driver.rb
@@ -0,0 +1,17 @@
+RSpec.configure do |config|
+  config.before :suite, js: :true do
+      require 'capybara/poltergeist'
+      Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(app, {
+          js_errors: true,
+          inspector: true,
+          phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'],
+          timeout: 120
+        })
+      end
+  end
+
+  config.before :each, js: :true do
+    Capybara.current_driver = :poltergeist
+  end
+end
