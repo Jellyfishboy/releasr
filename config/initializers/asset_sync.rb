9aacdfcd815497de7922aa4247042e23e8c0cc53
JellyFishBoy <tom.alan.dallimore@googlemail.com>
2014-06-24 00:19:41 +0100

    Modified asset_sync.

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,34 +1,36 @@
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.aws_access_key_id = Settings.aws.s3.id
-  config.aws_secret_access_key = Settings.aws.s3.key
-  # To use AWS reduced redundancy storage.
-  # config.aws_reduced_redundancy = true
-  config.fog_directory = Settings.aws.s3.bucket
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = 'AWS'
+    config.aws_access_key_id = Settings.aws.s3.id
+    config.aws_secret_access_key = Settings.aws.s3.key
+    # To use AWS reduced redundancy storage.
+    # config.aws_reduced_redundancy = true
+    config.fog_directory = Settings.aws.s3.bucket
 
-  config.fog_region = Settings.aws.s3.region
+    config.fog_region = Settings.aws.s3.region
 
-  config.existing_remote_files = "delete"
+    config.existing_remote_files = "delete"
 
-  config.gzip_compression = true
+    config.gzip_compression = true
 
-  # Invalidate a file on a cdn after uploading files
-  # config.cdn_distribution_id = "12345"
-  # config.invalidate = ['file1.js']
+    # Invalidate a file on a cdn after uploading files
+    # config.cdn_distribution_id = "12345"
+    # config.invalidate = ['file1.js']
 
-  # Increase upload performance by configuring your region
-  # config.fog_region = 'eu-west-1'
-  #
-  # Don't delete files from the store
-  # config.existing_remote_files = "keep"
-  #
-  # Automatically replace files with their equivalent gzip compressed version
-  # config.gzip_compression = true
-  #
-  # Use the Rails generated 'manifest.yml' file to produce the list of files to
-  # upload instead of searching the assets directory.
-  # config.manifest = true
-  #
-  # Fail silently.  Useful for environments such as Heroku
-  # config.fail_silently = true
+    # Increase upload performance by configuring your region
+    # config.fog_region = 'eu-west-1'
+    #
+    # Don't delete files from the store
+    # config.existing_remote_files = "keep"
+    #
+    # Automatically replace files with their equivalent gzip compressed version
+    # config.gzip_compression = true
+    #
+    # Use the Rails generated 'manifest.yml' file to produce the list of files to
+    # upload instead of searching the assets directory.
+    # config.manifest = true
+    #
+    # Fail silently.  Useful for environments such as Heroku
+    # config.fail_silently = true
+  end
 end
