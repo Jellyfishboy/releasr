09406ad11b258854fe0b7dde42c7a8c12e2978f3
Billy Dallimore <billy@sngtrkr.com>
2014-06-24 16:28:45 +0100

    Removed required validation for github field and modified notes icon.

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,10 +19,9 @@ class Project < ActiveRecord::Base
 
     has_many :releases
 
-    validates :name,                        uniqueness: { case_sensitive: false }
-    validates :name, :github,               presence: true
-    validates :github,                      format: { with: URI::regexp(%w(http https)), message: 'has an invalid URL. Please use HTTP procotol.' }
-    validates :website,                     format: { with: URI::regexp(%w(http https)), message: 'has an invalid URL. Please use HTTP procotol.' }, allow_blank: true
+    validates :name,                                uniqueness: { case_sensitive: false }
+    validates :name, :github,                       presence: true
+    validates :github, :website,                    format: { with: URI::regexp(%w(http https)), message: 'has an invalid URL. Please use HTTP procotol.' }, allow_blank: true    
 
     extend FriendlyId
     friendly_id :name, use: [:slugged, :finders]
