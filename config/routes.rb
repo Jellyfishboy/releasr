7fb199e935723298ccb05ee0ad1a5f5a36a75e3b
Billy Dallimore <billy@sngtrkr.com>
2014-06-24 17:24:56 +0100

    Added 404 and 500 pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Releasr::Application.routes.draw do
 
   
     root to: 'home#index'
+    
     resources :projects, only: :show do
         resources :releases, only: :show
     end
+
     devise_for :users, controllers: { sessions: "users/sessions" }
+
+    %w( 404 422 500 ).each do |code|
+        get code, :to => "errors#show", :code => code
+    end
 
     namespace :admin do
         root to: 'projects#index'
